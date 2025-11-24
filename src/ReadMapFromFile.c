09c0378f9f769ae2bcb1a26d062cc2e6e4adbdf7
TheCookieLegend <lucas2.olafsen@gmail.com>
2025-11-24 19:55:34 +0100

    Function explanations and comments

--- src/ReadMapFromFile.c
+++ src/ReadMapFromFile.c
@@ -32,7 +32,7 @@ int** CreateGridMap(int* px, int* py) {
         if (!grid[i]) { perror("malloc"); exit(EXIT_FAILURE); }
     }
 
-
+    FillGridFromFile(gridFile, y, x, grid);
     PrintGrid(y, x, grid);
 
 
@@ -41,6 +41,13 @@ int** CreateGridMap(int* px, int* py) {
     return grid;
 }
 
+/*
+ *  GetWareHouseSize(FILE*, int*, int*)
+ *  This function reads the first two numbers in the text file
+ *  It then saves this into the x and y variables that are passed as pointers to the function
+ *  It also looks at the numbers it receives. If strtol() returns 0, it means that the char could not be converted to an int
+ *  And if one of the numbers are smaller than 3, it returns an error, as we want the grid to be at least 3x3
+*/
 void GetWarehouseSize(FILE* file, int* x, int* y) {
     char cX[4], cY[4]; // This makes it possible for a warehouse size of 9999 x 9999
     fscanf(file, "%s %s", cX, cY); // Scans the first two integers as strings
@@ -58,6 +65,10 @@ void GetWarehouseSize(FILE* file, int* x, int* y) {
     }
 }
 
+/*
+ *  This just prints out the grid to the console
+ *  Mostly used for testing and wont be relevant for the final program
+*/
 void PrintGrid(int y, int x ,int **grid) {
     for (int i = 0; i < y; i++) {
         for (int j = 0; j < x; j++) {
@@ -67,9 +78,39 @@ void PrintGrid(int y, int x ,int **grid) {
     }
 }
 
+/*
+ *  When using malloc, it is important to free up the memory that has been allocated, so we don't get a memory leak
+ *  This function frees the memory
+*/
 void DeleteGrid(int y, int x, int **grid) {
-    for (int i = 0; i < x; i++) {
+    for (int i = 0; i < y; i++) {
         free(grid[i]);
     }
     free(grid);
+}
+
+/*
+ *  This function is pivotal for the program
+ *  It reads the file and fills out our 2D array with the info from the text file
+*/
+void FillGridFromFile(FILE* file, int y, int x, int **grid) {
+    int row = 0, col = 0; // Used to keep track of where we are in the array
+    char c;
+
+    while ((c = fgetc(file)) != EOF) {
+        if (c == '\n' || c == '\r') { // In windows, line breaks can be both \n and \r. But on mac, it only uses \r. So this is just to make sure that it works for any operating system
+            if (col > 0) {  // Sometimes the first character on a new line is \n. We do this to make sure that we only go to the next row, if we've actually read the line
+                row++;
+                col = 0;
+            }
+        } else {
+            if (row < y && col < x) { // Makes sure we dont go out of bounds
+                grid[row][col] = c - '0';  // Converts a character into an int using ASCII code. Only works for digits, so 0-9
+                col++;
+            } else {
+                perror("Program went out of bounds while trying to read text file. Check if text file is correct");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 }
