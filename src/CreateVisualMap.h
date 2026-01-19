8d0b061191988ef2ec9cfc954c537fdf4e064972
TheCookieLeg <lucas2.olafsen@gmail.com>
2026-01-19 12:53:03 +0100

    Updated example

--- src/CreateVisualMap.h
+++ src/CreateVisualMap.h
@@ -5,7 +5,7 @@
 #ifndef P1_PRODUCT_CREATEVISUALMAP_H
 #define P1_PRODUCT_CREATEVISUALMAP_H
 
-#define GRID_SIZE 30
+#define GRID_SIZE 60
 #define SLEEP_TIME 200000
 void CreateWarehouseWindow(int x, int y, int** grid);
 struct Color NumberToColor(int x);
